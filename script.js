7dec878715ba9de1f54fcdb6c4a36f24bd8c3cb8
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-12 21:46:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("ContactForm").onsubmit = function () {
+  alert("Your inquiry has been submitted. ");
+  return false; 
+};
+
+document.getElementsByTagName("tr")[1].onclick = function () {
+  alert("Tuesday class a 2- 4 PM in room B19.131");
+};
