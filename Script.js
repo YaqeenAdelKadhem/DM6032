490f1d2e36a61e43e9f51bb12a1299b5a9855bc9
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-17 21:06:52 +0300

    Add event handlers for contact form and table row
    Fixes a typo in the return statement and adds event handlers for form submission and table row click.

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactform").onsubmit=function(){alert("Your inquire has been submitted")};
+retuen false;};
+document.getElementsByTagName("tr")[1].onclick = function () {
+  alert("CupCake Sold Out");
+};
