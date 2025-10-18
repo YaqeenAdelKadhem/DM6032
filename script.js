762273fbfb935bac5fdd77e333bfe83de96994a2
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-18 16:00:10 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("contactForm").onsubmit = function () { 
-alert("Your inquiry has been submitted."); 
-return false;  
-}; 
-document.getElementsByTagName("tr")[1].onclick = function () { 
-alert("CupCake Sold Out"); 
-};
