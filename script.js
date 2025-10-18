65246beb4bfb907adcead581b244dd24460ec5d7
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-18 17:08:57 +0300

    Correct typos in event handling functions
    Fix typos in JavaScript function definitions and event handlers.

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getelementById("contactForm").onsubmit=funiction(){alret ("your inquire has been submitted."); return false;};
+
+document.getelementbyTagName("tr")[1].onclick dunction(){alert("item is sold out");};
