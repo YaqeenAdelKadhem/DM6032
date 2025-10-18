6c216adc9b6f77a3df0659803e1ccbbfea231466
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-18 17:40:44 +0300

    Correct typos in form submission and click handlers
    Fix typos in event handler functions in script.js.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getelementById("contactForm").onsubmit=funiction(){alret ("your inquire has been submitted."); return false;};
 
-document.getelementbyTagName("tr")[1].onclick dunction(){alert("item is sold out");};
+document.getelementbyTagName("tr")[1].onclick function(){alert("item is sold out");};
