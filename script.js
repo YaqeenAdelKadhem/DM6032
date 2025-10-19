9444768b002b1e34ad0555cc1ef56dd91ca73d0b
YaqeenAdelKadhem <202304551@student.polytechnic.bh>
2025-10-19 17:21:28 +0300

    Add form submission and table row click handlers
    Fix typos in event handlers and improve functionality.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getelementById("contactForm").onsubmit=function(){alret("yout inquires has been submitted"); retuen false;};
+
+document.getelementByTagName("tr")[1].onclick=function(){alret("service is full booked.");};
+
+                                                        
